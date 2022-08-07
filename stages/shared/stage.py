83c0b1ac69bcf7f3b26a98d6c519df41c0b44365
rez <reza.amini.majd1379@gmail.com>
2022-08-07 20:29:43 +0430

    :seedling: add stage file

---
+++ stages/shared/stage.py
@@ -0,0 +1,14 @@
+class Stage:
+    STEP = 1
+
+    def __init__(self):
+        pass
+
+    def pre_run(self):
+        pass
+
+    def run(self):
+        pass
+
+    def after_run(self):
+        pass
